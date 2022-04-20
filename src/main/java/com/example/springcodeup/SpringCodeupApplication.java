7c12241327948f313ea18201a852bb4dccb935f9
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-20 14:01:33 -0500

    starting spring with hello Controller

---
+++ src/main/java/com/example/springcodeup/SpringCodeupApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcodeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodeupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCodeupApplication.class, args);
+    }
+
+}
