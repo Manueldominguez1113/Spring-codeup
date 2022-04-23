7377beaf3bcf8790cc10632d30d69d288b558336
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-23 10:24:57 -0500

    break

--- src/main/java/com/example/springcodeup/controllers/PostController.java
+++ src/main/java/com/example/springcodeup/controllers/PostController.java
@@ -1,29 +1,45 @@
 package com.example.springcodeup.controllers;
 
+import com.example.springcodeup.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
+    public ArrayList<Post> example(){
+        ArrayList<Post> ex = new ArrayList<>();
+        Post one =new Post("hello!","First post");
+        Post two = new Post("hi..", "second post");
+        ex.add(one);
+        ex.add(two);
+        return ex;
+    }
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts(){
-        return "posts index page";
+    public String posts(Model model){
+        model.addAttribute("Posts", example());
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postIds(){
-        return "view an individual page";
+    public String postIds(@PathVariable Post id, Model model){
+        model.addAttribute("id",id);
+        return "posts/show";
     }
+
     @GetMapping("/posts/create")
     @ResponseBody
     public String startCreatePost(){
         return "view the form to create a post";
     }
+
     @PostMapping("/posts/create")
     @ResponseBody
     public String createPost(){
---
+++ src/main/java/com/example/springcodeup/controllers/welcomeController.java
@@ -0,0 +1,13 @@
+package com.example.springcodeup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class welcomeController {
+
+    @GetMapping("/welcome")
+    public String index(){
+        return "welcome";
+    }
+}
